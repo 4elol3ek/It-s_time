505488851591715a99603e5839e5ad5fb41faa0b
Struwer <Struwer427@yandex.ru>
2024-05-31 01:05:45 +0700

    add tests for lifeSimulation

--- test/lifeSimulation_test.c
+++ test/lifeSimulation_test.c
@@ -1,9 +1,29 @@
 #include <src/lifeSimulation.h>
 #include <stdio.h>
 
-void isEqual(int expected, int result, int number){
+void isEqual(int expected, int result, int number)
+{
     if(expected == result) printf("test%d: OK\n", number);
     else printf("test%d: FAIL\n", number);
+}
+
+//сравниваем посимвольно 2 поля
+int isEqualField(field expected, field result)
+{
+    int flag = 1;
+    for(int raw = 0; raw < 10; raw++)
+    {
+        for (int col = 0; col < 10; col++)
+        {
+            if(expected.arr[raw][col] != result.arr[raw][col])
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if(!flag) break;
+    }
+    return flag;
 }
 
 void testChangeCellWeight(){
@@ -20,8 +40,51 @@ void testChangeCellWeight(){
     isEqual(expected, result.arr[1][1], 2);
 }
 
+void testLifeSimulation(){
+    printf("lifeSimulation tests:\n");
+    //проверяем случай, кода у клетки нет соседей
+    field expected = makeNewField();
+    field result = makeNewField();
+    result = changeCellWeight(result, 2 , 2);
+    result = lifeSimulation(result);
+    isEqual(1,isEqualField(expected,result),1);
+
+    //проверяем случай, когда у клетки один сосед
+    expected = makeNewField();
+    result = makeNewField();
+    result = changeCellWeight(result, 2,2);
+    result = changeCellWeight(result, 2,3);
+    result = lifeSimulation(result);
+    isEqual(1,isEqualField(expected,result),1);
+
+    //проверяем случай, когда у клетки 2 соседа 
+    //заодно проверяем простейший пульсар
+    result = makeNewField();
+    result = changeCellWeight(result, 2,2);
+    result = changeCellWeight(result, 2,3);
+    result = changeCellWeight(result,2,4);
+    expected = result;
+    result = lifeSimulation(result);
+    result = lifeSimulation(result);
+    isEqual(1,isEqualField(expected,result),1);
+    
+    //проверяем статичную фигуру
+    //не знаю зачем...
+    result = makeNewField();
+    result = changeCellWeight(result, 2,2);
+    result = changeCellWeight(result, 2,3);
+    result = changeCellWeight(result,3,2);
+    expected = result;
+    expected= changeCellWeight(expected, 3,3);
+    //клетки создают квадрат
+    result = lifeSimulation(result);
+    result = lifeSimulation(result);
+    isEqual(1,isEqualField(expected,result),1);
+}
+
 int main(int argc, char const *argv[])
 {
     testChangeCellWeight();
+    testLifeSimulation();
     return 0;
 }
